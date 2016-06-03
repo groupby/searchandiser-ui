ea960aabdc8d1f556c7b2378387302f6907875ac
Ben Teichman <ben.l.teichman@gmail.com>
2016-06-03 11:21:06 -0400

    remove dist from VC

--- dist/definitions/searchandiser-ui.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../src/all.d.ts" />
-import './tags/gb-query.tag';
-import './tags/gb-results.tag';
-import './tags/gb-paging.tag';
-import './tags/gb-related-searches.tag';
-import './tags/gb-did-you-mean.tag';
-import './tags/gb-available-navigation.tag';
-import './tags/gb-selected-navigation.tag';
