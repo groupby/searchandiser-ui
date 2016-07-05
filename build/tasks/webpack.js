16bf14e4a1e642e0f496ae6d48bd54ae39693065
Ben Teichman <ben.l.teichman@gmail.com>
2016-07-05 11:22:24 -0400

    add webpack:minify task

--- build/tasks/webpack.js
+++ build/tasks/webpack.js
@@ -1,23 +1,30 @@
 var gulp = require('gulp'),
+  wp = require('webpack'),
   webpack = require('webpack-stream'),
   pjson = require('../../package.json'),
-  paths = require('../paths.js');
+  packConfig = require('../../webpack.conf'),
+  paths = require('../paths.js'),
+  assign = require('object-assign');
 
 gulp.task('webpack:bundle', function() {
   return gulp.src(paths.src + '/index.ts')
-    .pipe(webpack(require('../../webpack.conf')))
-    .pipe(gulp.dest('dist/'));
+    .pipe(webpack(packConfig))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('webpack:minify', function() {
+  var minConfig = assign({}, packConfig, {
+    output: { filename: pjson.name + '-' + pjson.version + '.min.js' },
+    plugins: [new wp.optimize.UglifyJsPlugin()]
+  });
+
+  return gulp.src('')
+    .pipe(webpack(minConfig))
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('webpack:watch', function() {
   return gulp.src(paths.src + '/index.ts')
-    .pipe(webpack({
-      watch: true,
-      output: { filename: pjson.name + '-' + pjson.version + '.js' },
-      resolve: { extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'] },
-      module: {
-        loaders: [{ test: /\.ts$/, loader: 'ts-loader' }]
-      }
-    }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(webpack(assign({}, packConfig, { watch: true })))
+    .pipe(gulp.dest(paths.dist));
 });
