abfcc9fd1c9ab9cd287dca491abf100061ebabd6
Eric Hacke <eric.hacke@groupbyinc.com>
2016-07-21 11:39:49 -0400

    Hacked

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,24 @@
 export function toRefinement(ref, nav) {
-  return Object.assign({}, pluck(ref, 'type', 'value', 'low', 'high'), { navigationName: nav.name });
+  return Object.assign({}, pluck(ref, 'type', 'value', 'low', 'high'), {navigationName: nav.name});
 }
 
 export function displayRefinement(ref) {
-  return ref.type === 'Value' ? ref.value : `${ref.low} - ${ref.high}`;
+  let value = null;
+  if (typeof ref.value === 'string') {
+    const split = ref.value.split('>');
+    value = camelize(split[split.length - 1]);
+  } else {
+    value = ref.value;
+  }
+
+  return ref.type === 'Value' ? value : `${ref.low} - ${ref.high}`;
 }
 
-export function pluck(obj: any, ...keys: string[]): any {
-  return keys.reduce((res, key) => obj[key] ? Object.assign(res, { [key]: obj[key] }) : res, {});
+export function pluck(obj:any, ...keys:string[]):any {
+  return keys.reduce((res, key) => obj[key] ? Object.assign(res, {[key]: obj[key]}) : res, {});
 }
 
-export function checkNested(obj: any, ...keys: string[]): boolean {
+export function checkNested(obj:any, ...keys:string[]):boolean {
   return Array.prototype.slice.call(arguments, 1)
     .reduce((res, arg) => {
       if (!obj || !obj.hasOwnProperty(arg)) return false;
@@ -19,7 +27,7 @@ export function checkNested(obj: any, ...keys: string[]): boolean {
     }, true);
 }
 
-export function getParam(param): string | null {
+export function getParam(param):string | null {
   const queryParams = window.location.search
     .substring(1)
     .split('&');
@@ -27,4 +35,8 @@ export function getParam(param): string | null {
   return index !== -1 && queryParams[index].includes('=') ? queryParams[index].split('=')[1] : null;
 }
 
-export const unless = (obj: any, defaultObj: any) => obj == undefined ? defaultObj : obj;
+export const unless = (obj:any, defaultObj:any) => obj == undefined ? defaultObj : obj;
+
+function camelize(str) {
+  return str.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+}
