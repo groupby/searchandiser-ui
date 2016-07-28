58dda0426b567904fc12c5398f73ee57577798ba
Ben Teichman <ben.l.teichman@gmail.com>
2016-07-28 00:02:27 -0600

    page-size mixin

---
+++ src/tags/page-size/gb-page-size.ts
@@ -0,0 +1,18 @@
+import { FluxTag } from '../tag';
+
+export interface PageSize extends FluxTag { }
+
+export class PageSize {
+
+  passthrough: any;
+
+  init() {
+    const parentOpts = this.opts.passthrough || this.opts;
+    this.passthrough = Object.assign({}, parentOpts.__proto__, {
+      options: parentOpts.config.pageSizes || [10, 25, 50, 100],
+      hover: parentOpts.onHover,
+      update: (value) => parentOpts.flux.resize(value, parentOpts.resetOffset ? 0 : undefined),
+      default: true
+    });
+  }
+}
--- src/tags/page-size/gb-raw-page-size.tag
+++ src/tags/page-size/gb-raw-page-size.tag
@@ -4,12 +4,7 @@
   </gb-raw-select>
 
   <script>
-    const parentOpts = opts.passthrough || opts;
-    this.passthrough = Object.assign({}, parentOpts.__proto__, {
-      options: parentOpts.config.pageSizes || [10, 25, 50, 100],
-      hover: parentOpts.onHover,
-      update: (value) => parentOpts.flux.resize(value, parentOpts.resetOffset ? 0 : undefined),
-      default: true
-    });
+    import { PageSize } from './gb-page-size';
+    this.mixin(new PageSize().__proto__);
   </script>
 </gb-raw-page-size>
