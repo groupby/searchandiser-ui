be74b684de36f29aeba3b9d5948001c3e15dfc0f
Ben Teichman <ben.l.teichman@gmail.com>
2016-07-29 11:29:31 -0600

    paging controller is now a class

--- src/tags/paging/gb-paging.ts
+++ src/tags/paging/gb-paging.ts
@@ -1,8 +1,44 @@
+import { FluxTag } from '../tag';
 import { unless } from '../../utils';
 import { Events } from 'groupby-api';
 
-export function Paging() {
-  this.init = function() {
+export interface Labeled {
+  prev_label: string;
+  next_label: string;
+  first_label: string;
+  last_label: string;
+}
+
+export interface Paging extends FluxTag, Labeled {
+  parent: Riot.Tag.Instance & Labeled & { style: Function };
+}
+
+export class Paging {
+
+  limit: number;
+  pages: boolean;
+  numeric: boolean;
+  terminals: boolean;
+  icons: boolean;
+  forwardDisabled: boolean;
+  backDisabled: boolean;
+  style: Function;
+
+  pageNumbers: number[];
+  currentPage: number;
+  lastPage: number;
+  lowOverflow: boolean;
+  highOverflow: boolean;
+
+  pager: {
+    first: () => void;
+    prev: () => void;
+    next: () => void;
+    last: () => void;
+    jump: (number) => void;
+  }
+
+  init() {
     this.limit = unless(this.opts.limit, 5);
     this.style = unless(this.opts.style, this.parent.style);
     this.pages = unless(this.opts.pages, false);
@@ -25,9 +61,9 @@ export function Paging() {
 
     this.opts.flux.on(Events.PAGE_CHANGED, this.updatePages);
     this.opts.flux.on(Events.RESULTS, this.updatePageDisplay);
-  };
+  }
 
-  this.updatePageDisplay = function() {
+  updatePageDisplay() {
     const pageNumbers = this.opts.flux.page.pageNumbers(this.limit);
     const lastPage = this.opts.flux.page.finalPage + 1;
     this.update({
@@ -35,9 +71,9 @@ export function Paging() {
       lowOverflow: pageNumbers[0] !== 1,
       highOverflow: pageNumbers[pageNumbers.length - 1] !== lastPage
     });
-  };
+  }
 
-  this.updatePages = function({ pageIndex, finalPage }) {
+  updatePages({ pageIndex, finalPage }) {
     this.update({
       currentPage: pageIndex + 1,
       lastPage: finalPage + 1,
