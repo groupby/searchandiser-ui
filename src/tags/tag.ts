f7314c96cd440bfcb31dbcf863662458b1931c33
Ben Teichman <ben.l.teichman@gmail.com>
2016-08-05 09:51:54 -0400

    add findParent() to root tag mixin

--- src/tags/tag.ts
+++ src/tags/tag.ts
@@ -14,6 +14,11 @@ export function RootTag(flux: FluxCapacitor, config: any) {
   return {
     flux, config,
     _style: config.stylish ? 'gb-stylish' : '',
-    _clone: () => initCapacitor(Object.assign({}, config, { initialSearch: false }))
+    _clone: () => initCapacitor(Object.assign({}, config, { initialSearch: false })),
+    findParent: (tag: Riot.Tag.Instance, name: string) => {
+      let parentTag: Riot.Tag.Instance = tag;
+      while (parentTag.root.localName !== name && parentTag.parent) parentTag = parentTag.parent;
+      return parentTag;
+    }
   };
 }
