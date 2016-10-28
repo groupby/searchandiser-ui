189f46688b163675ece35dfebcb3678547e932c3
Victoria Johns <vmichellej@gmail.com>
2016-10-28 17:04:44 -0400

    f

--- test/e2e/tests/paging.js
+++ test/e2e/tests/paging.js
@@ -1,12 +1,13 @@
 module.exports = {
   'go to next page': (browser) => {
     browser.url('http://localhost:8080')
-      .waitForElementVisible('body', 1000)
       // .setValue('input[type=text]', 'nightwatch')
-      // .waitForElementVisible('button[name=btnG]', 1000)
-      // .click('button[name=btnG]')
-      // .pause(1000)
+      .waitForElementVisible('.gb-pager__link.next', 1000)
+      .pause(500)
+      .click('.gb-pager__link.next')
       // .assert.containsText('#main', 'Night Watch')
+      .pause(500)
+      .assert.cssClassNotPresent('.gb-pager__link.prev', 'disabled')
       .end();
   }
 };
