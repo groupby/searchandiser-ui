39e75d87e435d6a5189a5a6476ea81c2196eef4b
Ben Teichman <ben.l.teichman@gmail.com>
2016-10-31 10:45:04 -0400

    missed functional test context

--- karma.entry.ts
+++ karma.entry.ts
@@ -3,8 +3,8 @@ import './test/bootstrap';
 const coreContext = (<{ context?: Function }>require).context('./src', true, /\.ts/);
 coreContext.keys().forEach(coreContext);
 
-const unitTestContext = (<{ context?: Function }>require).context('./test/unit', true, /\.ts/);
-unitTestContext.keys().forEach(unitTestContext);
-
 const functionalTestContext = (<{ context?: Function }>require).context('./test/functional', true, /\.ts/);
+functionalTestContext.keys().forEach(functionalTestContext);
+
+const unitTestContext = (<{ context?: Function }>require).context('./test/unit', true, /\.ts/);
 unitTestContext.keys().forEach(unitTestContext);
