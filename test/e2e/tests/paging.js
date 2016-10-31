7bb730781e92efc9371d51fa0d9b4c0fdf18bde9
Ben Teichman <ben.l.teichman@gmail.com>
2016-10-31 10:13:10 -0400

    try testing

--- test/e2e/tests/paging.js
+++ test/e2e/tests/paging.js
@@ -1,13 +1,54 @@
 module.exports = {
+  'initial state': (browser) => {
+    browser.url('http://localhost:8080')
+      .waitForElementVisible('.gb-pager__link.next', 100)
+      .pause(500) // wait for riot to render results
+      .expect.element('.gb-pager__link.prev.disabled').to.be.present;
+    browser.expect.element('.gb-terminal__link.first.disabled').to.be.present;
+    browser.expect.element('.gb-terminal__link.last:not(.disabled)').to.be.present;
+    browser.expect.element('.gb-pager__link.next:not(.disabled)').to.be.present;
+    browser.expect.element('.gb-pages__page.selected').text.to.eq('1');
+    browser.end();
+  },
+
   'go to next page': (browser) => {
     browser.url('http://localhost:8080')
-      // .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('.gb-pager__link.next', 1000)
-      .pause(500)
+      .waitForElementVisible('.gb-pager__link.next', 100)
+      .pause(500) // wait for riot to render results
       .click('.gb-pager__link.next')
-      // .assert.containsText('#main', 'Night Watch')
-      .pause(500)
-      .assert.cssClassNotPresent('.gb-pager__link.prev', 'disabled')
-      .end();
+      .expect.element('.gb-pager__link.prev:not(.disabled)').to.be.present.before(1000);
+    browser.expect.element('.gb-terminal__link.first:not(.disabled)').to.be.present;
+    browser.expect.element('.gb-pages__page.selected').text.to.eq('2');
+    browser.end();
+  },
+
+  'go to previous page': '' + function(browser) {
+    browser.url('http://localhost:8080')
+      .waitForElementVisible('.gb-pager__link.prev', 100)
+      .pause(500) // wait for riot to render results
+      .click('.gb-pager__link.prev')
+      .expect.element('.gb-pager__link.prev.disabled').to.be.present.before(1000);
+    browser.expect.element('.gb-terminal__link.first.disabled').to.be.present;
+    browser.end();
+  },
+
+  'go to last page': (browser) => {
+    browser.url('http://localhost:8080')
+      .waitForElementVisible('.gb-terminal__link.last', 100)
+      .pause(500) // wait for riot to render results
+      .click('.gb-terminal__link.last')
+      .expect.element('.gb-pager__link.next:not(.disabled)').to.be.present.before(1000);
+    browser.expect.element('.gb-terminal__link.last:not(.disabled)').to.be.present;
+    browser.end();
+  },
+
+  'go to first page': '' + function(browser) {
+    browser.url('http://localhost:8080')
+      .waitForElementVisible('.gb-terminal__link.first', 100)
+      .pause(500) // wait for riot to render results
+      .click('.gb-terminal__link.first')
+      .expect.element('.gb-pager__link.prev.disabled').to.be.present.before(1000);
+    browser.expect.element('.gb-terminal__link.first.disabled').to.be.present;
+    browser.end();
   }
 };
