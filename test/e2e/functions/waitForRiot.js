8d2ba8101ee5c11caaceb70347bb98eeac4d4347
Ben Teichman <ben.l.teichman@gmail.com>
2016-12-20 00:39:11 -0500

    temporary measures

--- test/e2e/functions/waitForRiot.js
+++ test/e2e/functions/waitForRiot.js
@@ -2,63 +2,87 @@
 const util = require('util');
 const events = require('events');
 
+const FIREFOX_TIMEOUT = 1000;
 const DEFAULT_TIMEOUT = 500;
+const CHECK_TIMEOUT = 25;
 
 function WaitForRiot() {
   events.EventEmitter.call(this);
+  this.startTime = null;
 }
 
 util.inherits(WaitForRiot, events.EventEmitter);
 
-WaitForRiot.prototype.command = function(msOrCb, cb) {
-  const hasMs = typeof msOrCb !== 'number';
-  const ms = hasMs && msOrCb || DEFAULT_TIMEOUT;
-  cb = hasMs ? msOrCb : cb;
+WaitForRiot.prototype.command = function(timeout) {
 
-  // const scheduledSetup = setInterval(() => {
-  //   this.client.api.execute(function() {
-  //     return window.searchandiser.flux !== undefined;
-  //   }, [], (res) => {
-  //     if (res.value) {
-  //       clearInterval(scheduledSetup);
-  //       this.client.api.execute(function() {
-  //         window.searchandiser.flux.once('results', function() {
-  //           window.searchandiser.__loaded__ = true;
-  //         });
-  //       }, [], () => this.checkLoad(ms, cb));
-  //     }
-  //   });
-  // }, 10);
-  setTimeout(() => {
-    this.emit('complete');
-  }, DEFAULT_TIMEOUT);
+  if (this.api.capabilities.browserName === 'firefox') {
+    setTimeout(() => {
+      this.client.assertion(true, '', 'riot loaded', `waitForRiot() defaulted to ${FIREFOX_TIMEOUT}ms because firefox`, true);
+      this.emit('complete');
+    }, FIREFOX_TIMEOUT);
+  } else {
+    let message = null;
+    this.startTime = new Date().getTime();
+    timeout = timeout || DEFAULT_TIMEOUT;
+
+    this.setup((setupComplete, configuredTime) => {
+      if (setupComplete) {
+        message = `searchandiser was configured after ${configuredTime - this.startTime} ms.`;
+      } else {
+        message = `searchandiser was still not configured after ${timeout} ms.`;
+      }
+      this.client.assertion(setupComplete, 'searchandiser not configured', 'searchandiser configured', message, true);
+
+      this.check((result, loadedTime) => {
+        if (result) {
+          message = `riot loaded after ${loadedTime - this.startTime} ms.`;
+        } else {
+          message = `riot was still not loaded after ${timeout} ms.`;
+        }
+        this.client.assertion(result, 'riot not loaded', 'riot loaded', message, true);
+        this.emit('complete');
+      }, timeout, CHECK_TIMEOUT);
+    }, timeout, CHECK_TIMEOUT);
+  }
 
   return this;
 };
 
-WaitForRiot.prototype.checkLoad = function(ms, cb) {
-  let elapsed = 0;
-  const scheduledCheck = setInterval(() => {
-    this.client.api.execute(function() {
-      return window.searchandiser.__loaded__;
-    }, [], (res) => {
-      if (res.value) {
-        clearInterval(scheduledCheck);
-
-        if (cb) {
-          cb.call(this.client.api);
-        }
-        this.emit('complete');
-      }
-    });
-
-    if (elapsed >= ms) {
-      clearInterval(scheduledCheck);
-      this.client.assertion(false, 'loaded', 'not loaded', 'expected riot to be loaded');
+WaitForRiot.prototype.setup = function(callback, maxTime, testTime) {
+  waitFor(new Promise((resolve) => this.api.execute(function() {
+    return window.searchandiser.flux !== undefined;
+  }, [], (result) => resolve(result.value))), (complete, time) => {
+    if (complete) {
+      this.api.execute(function() {
+        return window.searchandiser.flux.on('results', function() {
+          window.searchandiser.__loaded__ = true;
+        });
+      }, [], () => callback(true, time));
+    } else {
+      callback(false);
     }
-
-    elapsed++;
-  }, 10);
+  }, maxTime, testTime);
 };
+
+WaitForRiot.prototype.check = function(callback, maxTime, testTime) {
+  waitFor(new Promise((resolve) => this.api.execute(function() {
+    return window.searchandiser.__loaded__;
+  }, [], (result) => resolve(result.value))), callback, maxTime, testTime);
+};
+
+function waitFor(promise, callback, maxTime, testTime) {
+  promise
+    .then((complete) => {
+      const now = new Date().getTime();
+      if (complete) {
+        callback(true, now);
+      } else if (now - this.startTime < maxTime) {
+        setTimeout(() => this.check(callback, maxTime), testTime);
+      } else {
+        callback(false);
+      }
+    })
+    .catch(() => setTimeout(() => this.check(callback, maxTime), testTime));
+}
 
 module.exports = WaitForRiot;
