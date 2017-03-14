32d44edb7eb26a012ce2ece9bab88539543199f0
Ben Teichman <ben.l.teichman@gmail.com>
2017-03-14 11:09:48 -0400

    remove old refinement components

--- src/tags/navigation/index.ts
+++ src/tags/navigation/index.ts
@@ -1,7 +1,5 @@
 import './gb-and-refinement.tag.html';
-import './gb-available-refinement.tag.html';
 import './gb-more-refinements.tag.html';
 import './gb-navigation.tag.html';
 import './gb-or-refinement.tag.html';
 import './gb-refinement-list.tag.html';
-import './gb-selected-refinement.tag.html';
