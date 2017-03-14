5c7827b767d15c8adc5d1539be66d38c829b66d5
Victoria Johns <vmichellej@gmail.com>
2017-03-14 11:31:54 -0400

    Condense refinement tags

--- src/tags/navigation/index.ts
+++ src/tags/navigation/index.ts
@@ -1,5 +1,4 @@
-import './gb-and-refinement.tag.html';
 import './gb-more-refinements.tag.html';
 import './gb-navigation.tag.html';
-import './gb-or-refinement.tag.html';
 import './gb-refinement-list.tag.html';
+import './gb-refinement.tag.html';
